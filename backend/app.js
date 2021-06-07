55b8ed4e11bd3892b493a24cd8a81e62f3acde5c
Nadun Sandeepa <68732378+IT19245584@users.noreply.github.com>
2021-06-07 14:46:19 +0530

    three

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 6060;
+
+app.use(cors());
+app.use(express.json());
+
+const url = process.env.ATLAS_URI;
+global.URL = url;
+
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true,  useUnifiedTopology: true});
+
+const connection = mongoose.connection;
+
+connection.once('open',()=>{
+    console.log("MongoDB connection successfully");
+});
+
+// const systemReg = require('./routes/systemReg');
+// app.use('/systemReg', systemReg);
+
+
+
+app.listen(port,() =>{
+    console.log(`Server is running on port: ${port}`);
+});
---
+++ backend/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const systemRegSchema = new Schema({
+    username: {
+    type: String,
+    required: true,
+    unique: true
+    },
+    password: {
+    type: String,
+    required: true
+    },
+    password: {
+    type: String,
+    required: true
+    },
+    type: {
+    type: String,
+    required: true
+    },
+}, {
+timestamps: true
+});
+const system_Reg_Schema = mongoose.model('system_reg', systemRegSchema);
+module.exports = system_Reg_Schema;
