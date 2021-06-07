e80f32ee07e50a654241f0beac2d4cf3e4b8cfbe
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-06-07 22:26:20 +0530

    Add UI of committee member pages

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Index from './components/LandingPage/index';
-
+import OrganizingCommittee from './components/LandingPage/Committee/OrganizingCommittee';
+import TechnicalCommittee from './components/LandingPage/Committee/TechnicalCommittee';
 
 
 function App() {
-  return (
-  <div>
-       <Router>
-            <Switch>
-                <Route exact path="/">
-                    <Index/>
-                </Route>
-            </Switch>
-      </Router>
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route exact path="/"><Index /></Route>
+                    <Route exact path='/org'><OrganizingCommittee /></Route>
+                    <Route exact path='/tec'><TechnicalCommittee /></Route>
+                </Switch>
+            </Router>
 
-  </div>
+        </div>
     );
 }
 
---
+++ frontend/components/LandingPage/Committee/TechnicalCommittee.jsx
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import HomePageNav from '../../navbars/homePageNav';
+import Footer from '../../footer/footer';
+
+class TechnicalCommittee extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            Name: "Deepika Srinivasan",
+            Department: "Department of Computer Science",
+            Description: "Sample description "
+        }
+    }
+    render() {
+        return (
+            <div>
+                <HomePageNav />
+                <div>
+                    <div className="d-flex p-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
+                        <h1 tag='div' className='display-1 pb-3 mb-3 border-bottom'>Technical Committee</h1>
+                    </div>
+                </div>
+                <div>
+                    <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '70rem', margin: 'auto', padding: '10px' }}>
+                        <div className="card-body" >
+                            <h5 className="card-title" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '45px' }}>{this.state.Name}</h5>
+                            <br></br>
+                            <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
+                                <div className="card-body  hover-zoom" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
+                                    <img src="https://mdbootstrap.com/img/logo/mdb192x192.jpg" class="img-fluid rounded-circle hover-shadow" />
+                                </div>
+                            </div>
+                            <br></br>
+                            <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '25px' }}>{this.state.Department}</h3>
+                            <div className="card-text" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '23px' }}>
+                                <p>{this.state.Description}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <br></br>
+                </div>
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default TechnicalCommittee;
+
