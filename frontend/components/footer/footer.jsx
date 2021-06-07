7aaa17996387d6c79ef56f8e66b920edd8fd4667
Nadun Sandeepa <68732378+IT19245584@users.noreply.github.com>
2021-06-07 17:08:12 +0530

    elvenss

---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,32 @@
+import React ,{useState , useEffect} from "react";
+
+function HomeNavBar() {
+  return (
+        <div>
+       <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+                <a class="navbar-brand text-muted" href="#">AFGlobal-2021</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                    <a class="nav-link " aria-current="page" href="#">Privacy Policy</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Help</a>
+                    </li>
+                  
+                </ul>
+                </div>
+            </div>
+            </nav>
+        </div>
+    );
+}
+
+export default HomeNavBar;
