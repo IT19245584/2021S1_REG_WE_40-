2fec1b81eadb187c2572d9c02764c852bfc129fc
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-06-08 12:03:33 +0530

    Add new route in app.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Index from './components/LandingPage/index';
 import OrganizingCommittee from './components/LandingPage/Committee/OrganizingCommittee';
 import TechnicalCommittee from './components/LandingPage/Committee/TechnicalCommittee';
-
+import ContactUs from './components/LandingPage/ContactUs/ContactUs';
 
 function App() {
     return (
@@ -13,9 +13,9 @@ function App() {
                     <Route exact path="/"><Index /></Route>
                     <Route exact path='/org'><OrganizingCommittee /></Route>
                     <Route exact path='/tec'><TechnicalCommittee /></Route>
+                    <Route exact path='/contact'><ContactUs/></Route>
                 </Switch>
             </Router>
-
         </div>
     );
 }
