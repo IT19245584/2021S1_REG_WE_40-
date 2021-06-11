2e0d25450cd1610a0f20c1ce86258f2fd4b68ddd
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-06-11 10:47:12 +0530

    Change UI of KeyNotes

--- frontend/components/LandingPage/KeyNotes/GuestSpeaker.jsx
+++ frontend/components/LandingPage/KeyNotes/GuestSpeaker.jsx
@@ -11,22 +11,57 @@ class GuestSpeaker extends Component {
     }
     render() {
         return (
-            <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '70rem', margin: 'auto', padding: '10px' }}>
-                <div className="card-body" >
-                    <h5 className="card-title" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '45px' }}>{this.state.Name}</h5>
-                    <br></br>
-                    <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
-                        <div className="card-body  hover-zoom" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
-                            <img src="https://mdbootstrap.com/img/logo/mdb192x192.jpg" class="img-fluid rounded-circle hover-shadow" />
+            <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '120rem', margin: 'auto', padding: '10px' }}>
+                        <div className="card-body" >
+                            <div class="row">
+                                <div class="col-sm-6 mb-5">
+                                    <h5 className="card-title" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '45px' }}>{this.state.Name}</h5>
+                                    <br></br>
+                                    <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
+                                        <div className="card-body  hover-zoom" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
+                                            <img src="https://mdbootstrap.com/img/logo/mdb192x192.jpg" class="img-fluid rounded-circle hover-shadow" />
+                                        </div>
+                                    </div>
+                                    <br></br>
+                                    <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '25px' }}>{this.state.Department}</h3>
+                                    <div className="card-text" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '23px' }}>
+                                        <p>{this.state.Description}</p>
+
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 mb-5">
+                                    <h5 className="card-title" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '45px' }}>{this.state.Name}</h5>
+                                    <br></br>
+                                    <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
+                                        <div className="card-body  hover-zoom" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
+                                            <img src="https://mdbootstrap.com/img/logo/mdb192x192.jpg" class="img-fluid rounded-circle hover-shadow" />
+                                        </div>
+                                    </div>
+                                    <br></br>
+                                    <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '25px' }}>{this.state.Department}</h3>
+                                    <div className="card-text" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '23px' }}>
+                                        <p>{this.state.Description}</p>
+
+                                    </div>
+                                </div>
+                                <div class="col-sm-6 mb-5">
+                                    <h5 className="card-title" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '45px' }}>{this.state.Name}</h5>
+                                    <br></br>
+                                    <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
+                                        <div className="card-body  hover-zoom" style={{ maxWidth: '20rem', margin: '0 auto', padding: '10px', position: 'relative' }}>
+                                            <img src="https://mdbootstrap.com/img/logo/mdb192x192.jpg" class="img-fluid rounded-circle hover-shadow" />
+                                        </div>
+                                    </div>
+                                    <br></br>
+                                    <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '25px' }}>{this.state.Department}</h3>
+                                    <div className="card-text" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '23px' }}>
+                                        <p>{this.state.Description}</p>
+
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <br></br>
-                    <h3 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '25px' }}>{this.state.Department}</h3>
-                    <div className="card-text" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '23px' }}>
-                        <p>{this.state.Description}</p>
-                    </div>
-                </div>
-            </div>
         );
     }
 }
