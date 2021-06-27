070c56f2f6ddc6680314861fde68389491b82372
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-06-27 17:16:52 +0530

    Add API for view important dates

--- frontend/components/LandingPage/ImportantDates/ImportantDatesBody.jsx
+++ frontend/components/LandingPage/ImportantDates/ImportantDatesBody.jsx
@@ -1,58 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class ImportantDatedsBody extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            dates: []
+        }
     }
+
+    componentDidMount() {
+        axios.get('http://localhost:6060/importantDates/view')
+            .then(response => {
+                const dates = response.data;
+                this.setState({ dates });
+                console.log("response", response);
+            }).catch(error => {
+                alert(error.message);
+                console.log("Error", error);
+            });
+    }
+
     render() {
         return (
             <div>
                 <div className="card card border border-light shadow-0 mb-3" style={{ maxWidth: '100rem', margin: 'auto', padding: '10px' }}>
                     <div className="card-body" >
                         <div className="row">
-                            <div className="col-sm-6 mb-5">
-                                <div className="card " style={{ maxWidth: '50rem', margin: 'auto', padding: '10px' }}>
-                                    <div className="text-center">
-                                        <h1>Workshop paper submission</h1>
-                                    </div>
-                                    <div className="card-body">
-                                        <h3 className="card-title text-center text-capitalize">2021/07/05</h3>
-                                        <p className="card-text" style={{ fontSize: "30px" }}>
-                                            <span className="fw-bold">Due date : </span>2021/07/13<br />
-                                            <span className="fw-bold">Details : </span>Submit your workshop paper submition
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-sm-6 mb-5">
-                                <div className="card " style={{ maxWidth: '50rem', margin: 'auto', padding: '10px' }}>
-                                    <div className="text-center">
-                                        <h1>Workshop paper submission</h1>
-                                    </div>
-                                    <div className="card-body">
-                                        <h3 className="card-title text-center text-capitalize">2021/07/05</h3>
-                                        <p className="card-text" style={{ fontSize: "30px" }}>
-                                            <span className="fw-bold">Due date : </span>2021/07/13<br />
-                                            <span className="fw-bold">Details : </span>Submit your workshop paper submition
-                                        </p>
+                            {this.state?.dates?.length > 0 && this.state.dates.map((item, index) =>
+                                <div className="col-sm-6 mb-5" key={index}>
+                                    <div className="card " style={{ maxWidth: '50rem', margin: 'auto', padding: '10px' }}>
+                                        <div className="text-center">
+                                            <h1>{item.name}</h1>
+                                        </div>
+                                        <div className="card-body">
+                                            <h3 className="card-title text-center text-capitalize">{item.submitDate}</h3>
+                                            <p className="card-text" style={{ fontSize: "30px" }}>
+                                                <span className="fw-bold">Due date : </span>{item.dueDate}<br />
+                                                <span className="fw-bold">Details : </span>{item.description}
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="col-sm-6 mb-5">
-                                <div className="card " style={{ maxWidth: '50rem', margin: 'auto', padding: '10px' }}>
-                                    <div className="text-center">
-                                        <h1>Workshop paper submission</h1>
-                                    </div>
-                                    <div className="card-body">
-                                        <h3 className="card-title text-center text-capitalize">2021/07/05</h3>
-                                        <p className="card-text" style={{ fontSize: "30px" }}>
-                                            <span className="fw-bold">Due date : </span>2021/07/13<br />
-                                            <span className="fw-bold">Details : </span>Submit your workshop paper submition
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
+                            )}
                         </div>
                     </div>
                 </div>
