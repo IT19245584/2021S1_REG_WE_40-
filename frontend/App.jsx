26f6f42bbc0efe23306b2f45119de89257a302d9
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-06-27 22:48:01 +0530

    Resolve conflict

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -2,33 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './components/assert/css/Editor/editor.css';
 import Index from './components/LandingPage/index';
-
-import Reviewer_WorkShop_ViewTable from './components/Admin/Reviewer/reviewer_workshop_view_table';
-import Reviewer_WorkShop_ViewOne from './components/Admin/Reviewer/reviewer_workshop_view_one';
-import Reviewer_ResearchPaper_ViewTable from './components/Admin/Reviewer/reviewer_researchpaper_view_table';
-import Reviewer_ResearchPaper_ViewOne from './components/Admin/Reviewer/reviewer_researchpaper_view_one';
-import Reviewer_ResearchPaper_Edit from './components/Admin/Reviewer/reviewer_researchpaper_edit';
-
-
-function App() {
-  return (
-  <div>
-       <Router>
-            <Switch>
-                <Route exact path="/">
-                    <Index/>
-                </Route>
-                <Route path="/view-all-ws" component={Reviewer_WorkShop_ViewTable} />
-                <Route path="/view-all-rp" component={Reviewer_ResearchPaper_ViewTable} />
-                <Route path="/edit-researchpaper" component={Reviewer_ResearchPaper_Edit} />
-
-                <Route path="/view-ws" component={Reviewer_WorkShop_ViewOne} />
-                
-                <Route path="/view-researchpaper" component={Reviewer_ResearchPaper_ViewOne} />
-                
-            </Switch>
-      </Router>
-
 import OrganizingCommittee from './components/LandingPage/Committee/OrganizingCommittee';
 import TechnicalCommittee from './components/LandingPage/Committee/TechnicalCommittee';
 import ContactUs from './components/LandingPage/ContactUs/ContactUs';
@@ -46,6 +19,11 @@ import AddGuestSpeaker from './components/Admin/Editor/KeyNotes/GuestSpeakers/Ad
 import ViewImportantDates from './components/Admin/Editor/ImportantDates/ViewImportantDates';
 import ViewGuestSpeaker from './components/Admin/Editor/KeyNotes/GuestSpeakers/ViewGuestSpeaker';
 import ViewKeynoteSpeaker from './components/Admin/Editor/KeyNotes/KeyNoteSpeakers/ViewKeyNotes';
+import Reviewer_WorkShop_ViewTable from './components/Admin/Reviewer/reviewer_workshop_view_table';
+import Reviewer_WorkShop_ViewOne from './components/Admin/Reviewer/reviewer_workshop_view_one';
+import Reviewer_ResearchPaper_ViewTable from './components/Admin/Reviewer/reviewer_researchpaper_view_table';
+import Reviewer_ResearchPaper_ViewOne from './components/Admin/Reviewer/reviewer_researchpaper_view_one';
+import Reviewer_ResearchPaper_Edit from './components/Admin/Reviewer/reviewer_researchpaper_edit';
 
 function App() {
     return (
@@ -62,17 +40,23 @@ function App() {
                     <Route exact path='/date'><ImportantDate /></Route>
                     <Route exact path='/past'><PastProceeding /></Route>
 
+                    <Route path="/view-all-ws" component={Reviewer_WorkShop_ViewTable} />
+                    <Route path="/view-all-rp" component={Reviewer_ResearchPaper_ViewTable} />
+                    <Route path="/edit-researchpaper" component={Reviewer_ResearchPaper_Edit} />
+                    <Route path="/view-ws" component={Reviewer_WorkShop_ViewOne} />
+                    <Route path="/view-researchpaper" component={Reviewer_ResearchPaper_ViewOne} />
+
                     {/* Editor Pages */}
                     <Route exact path='/edhome'><EditorDashboard /></Route>
                     <Route exact path='/addorg'><AddOrg /></Route>
                     <Route exact path='/addtec'><AddTec /></Route>
-                    <Route exact path='/addabout'><AddAboutUs/></Route>
-                    <Route exact path='/adddate'><AddImportantDates/></Route>
-                    <Route exact path='/addkeynote'><AddKeyNotes/></Route>
-                    <Route exact path='/addguest'><AddGuestSpeaker/></Route>
-                    <Route exact path='/viewimpdate'><ViewImportantDates/></Route>
-                    <Route exact path='/viewguest'><ViewGuestSpeaker/></Route>
-                    <Route exact path='/viewkeynote'><ViewKeynoteSpeaker/></Route>
+                    <Route exact path='/addabout'><AddAboutUs /></Route>
+                    <Route exact path='/adddate'><AddImportantDates /></Route>
+                    <Route exact path='/addkeynote'><AddKeyNotes /></Route>
+                    <Route exact path='/addguest'><AddGuestSpeaker /></Route>
+                    <Route exact path='/viewimpdate'><ViewImportantDates /></Route>
+                    <Route exact path='/viewguest'><ViewGuestSpeaker /></Route>
+                    <Route exact path='/viewkeynote'><ViewKeynoteSpeaker /></Route>
 
 
                 </Switch>
