12d6d94de8ba2e1376eb1df26e9f4f9779cfc88e
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-06-28 20:44:10 +0530

    Add components to header

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,29 +1,28 @@
-import React ,{useState , useEffect} from "react";
+import React from "react";
 
 function HomeNavBar() {
-  return (
+    return (
         <div>
-       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div className="container-fluid">
-                <a className="navbar-brand text-muted" href="#">AFGlobal-2021</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                    <a className="nav-link " aria-current="page" href="#">Privacy Policy</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Contact Us</a>
-                    </li>
-                    <li className="nav-item">
-                    <a className="nav-link" href="#">Help</a>
-                    </li>
-                  
-                </ul>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark navbar-fixed-bottom">
+                <div className="container-fluid">
+                    <a className="navbar-brand text-muted" href="#">AFGlobal-2021</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <a className="nav-link " aria-current="page" href="#">Privacy Policy</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Contact Us</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">Help</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
             </nav>
         </div>
     );
