6b8c5a9a5022343b3f437262f40c651fa557ef41
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-06-29 16:22:07 +0530

    Add Past proceeding in user side

--- frontend/components/LandingPage/PastProceeding/PastProceedingHeading.jsx
+++ frontend/components/LandingPage/PastProceeding/PastProceedingHeading.jsx
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class PastProceedingHeading extends Component {
     render() { 
         return ( 
-            <div className="d-flex p-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '300px' }}>
-                <h1 tag='div' className='display-1 pb-3 mb-3 border-bottom'>Past Proceeding</h1>
+            <div className="d-flex p-2" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50px',marginBottom:'5px' ,marginTop:"80px"}}>
+                <h1 tag='div' className='display-1 pb-3 mb-3 border-bottom'>Past AFGlobal Conferences</h1>
             </div>
          );
     }
