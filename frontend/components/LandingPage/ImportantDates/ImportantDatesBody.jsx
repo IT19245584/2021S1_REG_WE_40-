ff1c5506da45b554140538d1797c951b480ec6c9
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-06-30 10:13:14 +0530

    Enable conditional rendering in user pages of editor part

--- frontend/components/LandingPage/ImportantDates/ImportantDatesBody.jsx
+++ frontend/components/LandingPage/ImportantDates/ImportantDatesBody.jsx
@@ -32,18 +32,20 @@ class ImportantDatedsBody extends Component {
                         <div className="row">
                             {this.state?.dates?.length > 0 && this.state.dates.map((item, index) =>
                                 <div className="col-sm-6 mb-5" key={index}>
-                                    <div className="card " style={{ maxWidth: '50rem', margin: 'auto', padding: '10px', borderRadius: '23px', background: '#ffffff', boxShadow: '5px 5px 34px #737373,-5px -5px 34px #ffffff' }}>
-                                        <div className="text-center">
-                                            <h3>{item.name}</h3>
+                                    {item.status && item.status === 'Post' &&
+                                        <div className="card " style={{ maxWidth: '50rem', margin: 'auto', padding: '10px', borderRadius: '23px', background: '#ffffff', boxShadow: '5px 5px 34px #737373,-5px -5px 34px #ffffff' }}>
+                                            <div className="text-center">
+                                                <h3>{item.name}</h3>
+                                            </div>
+                                            <div className="card-body">
+                                                <h3 className="card-title text-center text-capitalize">{item.submitDate}</h3>
+                                                <p className="card-text" style={{ fontSize: "20px" }}>
+                                                    <span className="fw-bold">Due date : </span>{item.dueDate}<br />
+                                                    <span className="fw-bold">Details : </span>{item.description}
+                                                </p>
+                                            </div>
                                         </div>
-                                        <div className="card-body">
-                                            <h3 className="card-title text-center text-capitalize">{item.submitDate}</h3>
-                                            <p className="card-text" style={{ fontSize: "20px" }}>
-                                                <span className="fw-bold">Due date : </span>{item.dueDate}<br />
-                                                <span className="fw-bold">Details : </span>{item.description}
-                                            </p>
-                                        </div>
-                                    </div>
+                                    }
                                 </div>
                             )}
                         </div>
