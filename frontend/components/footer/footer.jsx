c1d22b574c4ec5522f8f8c7a91921e53a4eccc93
DeepikaSharma5 <it19148632@my.sliit.lk>
2021-06-30 23:38:18 +0530

    Add admin bar

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -12,7 +12,10 @@ function HomeNavBar() {
                     <div className="collapse navbar-collapse" id="navbarNavDropdown">
                         <ul className="navbar-nav">
                             <li className="nav-item">
-                                <a className="nav-link" href="/contact">Contact Us</a>
+                                <a className="nav-link" href="/Admin_side_login">Admin Login</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/Admin_side_registration">Admin Register</a>
                             </li>
                         </ul>
                     </div>
